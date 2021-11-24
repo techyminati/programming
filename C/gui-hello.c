8a6587f1133b3e3b765899bba0e2f0beb53bd6c6
Aryan Sinha <sinha.aryan03@gmail.com>
2021-11-24 19:20:31 +0530

    programming: Write Basic GUI Program
    Tested-on: Windows 11 (64bit) - CodeBlocks IDE
    Tested-by: techyminati <sinha.aryan03@gmail.com>
    Signed-off-by: techyminati <sinha.aryan03@gmail.com>

---
+++ C/gui-hello.c
@@ -0,0 +1,11 @@
+// !/bin/bash
+// SPDX-License-Identifier: GPL-3.0-or-later
+// (c) TechyMinati ( Aryan Sinha ) <sinha.aryan03@gmail.com>
+
+#include <stdio.h>
+#include <windows.h>
+
+int main()
+{
+MessageBoxA( NULL, "Hello World!", "Hello World", MB_OK );
+}
